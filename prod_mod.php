76d40708d94aa556bad210ca2d8d0584b1c68a3e
cristian16b <cristian.budzicz@gmail.com>
2020-12-06 14:01:06 -0300

    Se agrega el editar

--- prod_mod.php
+++ prod_mod.php
@@ -14,17 +14,19 @@ $_POST = clean($_POST);
 $_GET = clean($_GET);
 
 if ($_POST[subgrabar]) {
-     $sql =  " update categoria set";
-     $sql .= " desc_cat = '".$_POST[categoria]."'";
-     $sql .= " where cod_cat = ".$_POST[cod_cat];
+	 mysql_query('BEGIN WORK');
+     $sql =  " update producto set";
+     $sql .= " descripcion = '".$_POST['categoria']."'";
+	 $sql .= " where id = ".$_POST['id'];
 
-     $res = mysql_query($sql);
-     $_GET[cod_cat]='';
+	 $res = mysql_query($sql);
+	 mysql_query('COMMIT');
+     $_GET['id']='';
 }
 
-if ($_GET[cod_cat]) {
+if ($_GET['id']) {
 
-	$sql = "select * from producto where id = ".$_GET[cod_cat];
+	$sql = "select * from producto where id = ".$_GET['id'];
 	$res = mysql_query($sql);
 	$row = mysql_fetch_array($res);
 
@@ -32,14 +34,14 @@ if ($_GET[cod_cat]) {
 
 	echo '<table cellspacing="0">';
 	echo '<tr>';
-	echo '<td>Nombre de categoria</td><td><input type=text name=categoria value="'.$row[desc_cat].'"></td>';
+	echo '<td>Nombre de categoria</td><td><input type=text name=categoria value="'.$row['descripcion'].'"></td>';
 	echo '</tr>';
 	echo '<tr>';
 	echo '<td colspan="2"><input value="Modificar" type="submit" name="subgrabar"></td>';
 	echo '</tr>';
 	echo '</table>';
 
-	echo '<input type=hidden name=cod_cat value='.$row[cod_cat].'>';
+	echo '<input type=hidden name=id value='.$row['id'].'>';
 
 	echo '</form>';
 
@@ -53,7 +55,7 @@ if ($_GET[cod_cat]) {
 		echo '<tr><td>Nombre del producto</td><td>ver detalle</td></tr>';
 		while ($row = mysql_fetch_array($res)) {
 			echo '<tr>';
-			echo '<td>'.$row['descripcion'].'</td><td><a href=prod_mod.php?cod_cat='.$row['id'].'>ver detalle</a></td>';
+			echo '<td>'.$row['descripcion'].'</td><td><a href=prod_mod.php?id='.$row['id'].'>ver detalle</a></td>';
 			echo '</tr>';
 		}
 		echo '</table>';
